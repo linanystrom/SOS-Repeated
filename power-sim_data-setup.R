cf83180e3003c8c6d93d17b4a9f601846638a5f0
Lina Nyström <lina.nystrom@psy.gu.se>
2022-10-20 14:23:05 +0200

    Simulation of effect sizes .30, .20, .15.

--- power-sim_data-setup.R
+++ power-sim_data-setup.R
@@ -6,13 +6,21 @@
 
 # Basic setup ------------------------------------------------------------------
 
-packages <- c("readr", "tibble", "dplyr", "data.table", "tidyr", "readxl", "ggplot2", "lme4", "simr")
+packages <- c("readr",
+              "tibble",
+              "dplyr",
+              "data.table",
+              "tidyr",
+              "readxl",
+              "ggplot2",
+              "lme4",
+              "simr")
 
 lapply(packages, library, character.only = TRUE)
 
 # Data structure ---------------------------------------------------------------
 
-group_n      <- 60
+group_n      <- 30
 
 n_phases     <- 5
 n_interviews <- 3
@@ -34,10 +42,13 @@ id <- sort(rep(1:(group_n * length(condition)), n_time))
 structure_full$id          <- id
 structure_full$condition   <- sort(rep(condition, group_n * n_time))
 structure_full$phase_sq    <- phase^2
-structure_full$condition <- factor(structure_full$condition, levels = c("direct", "standard"))
-# structure_full$information <- sample(1:5, nrow(structure_full), replace = TRUE)
+structure_full$condition <- factor(structure_full$condition,
+                                   levels = c("direct", "standard"))
 
-coeff <- c("(Intercept)" = 1.34,
+
+# Effect size 0.30--------------------------------------------------------------
+
+coeff_30 <- c("(Intercept)" = 1.34,
             "phase" = -0.45,
             "phase_sq" = -0.45,
             "interview" = 0.2,
@@ -50,14 +61,109 @@ coeff <- c("(Intercept)" = 1.34,
             "phase:interview:conditionstandard" = 0.3,
             "phase_sq:interview:conditionstandard" = 0.3)
 
-sim_lmer <- makeLmer(
-  formula = y ~ phase + phase_sq + interview + interview*condition + phase*interview + phase_sq*interview + phase*condition + phase_sq*condition + phase*interview*condition + phase_sq*interview*condition + (1|id), 
-  fixef = coeff,
+sim_lmer_30 <- makeLmer(
+  formula = y ~ phase
+  + phase_sq
+  + interview
+  + interview*condition
+  + phase*interview
+  + phase_sq*interview
+  + phase*condition
+  + phase_sq*condition
+  + phase*interview*condition
+  + phase_sq*interview*condition
+  + (1|id), 
+  fixef = coeff_30,
   VarCorr = 1.6,
   sigma = 1.2,
   data = structure_full
 )
 
-sim_1 <- powerSim(sim_lmer, test = fixed("phase_sq:interview:conditionstandard", method = "t"), nsim = 1000)
+sim_30 <- powerSim(sim_lmer_30,
+                   test = fixed("phase_sq:interview:conditionstandard",
+                   method = "t"),
+                   nsim = 1000)
 
-save(sim_1, file = "sim_sos.rda")
+save(sim_30, file = "sim_sos_30.rda")
+
+
+# Effect size 0.20--------------------------------------------------------------
+
+coeff_20 <- c("(Intercept)" = 1.34,
+              "phase" = -0.45,
+              "phase_sq" = -0.45,
+              "interview" = 0.2,
+              "conditionstandard" = 0.4,
+              "phase:interview" = 0,
+              "phase_sq:interview" = 0,
+              "phase:conditionstandard" = -0.2,
+              "phase_sq:conditionstandard" = -0.2,
+              "interview:conditionstandard" = 0.2,
+              "phase:interview:conditionstandard" = 0.3,
+              "phase_sq:interview:conditionstandard" = 0.2)
+
+sim_lmer_20 <- makeLmer(
+  formula = y ~ phase
+  + phase_sq
+  + interview
+  + interview*condition
+  + phase*interview
+  + phase_sq*interview
+  + phase*condition
+  + phase_sq*condition
+  + phase*interview*condition
+  + phase_sq*interview*condition
+  + (1|id), 
+  fixef = coeff_20,
+  VarCorr = 1.6,
+  sigma = 1.2,
+  data = structure_full
+)
+
+sim_20 <- powerSim(sim_lmer_20,
+                   test = fixed("phase_sq:interview:conditionstandard",
+                   method = "t"),
+                   nsim = 1000)
+
+save(sim_20, file = "sim_sos_20.rda")
+
+
+# Effect size 0.15--------------------------------------------------------------
+
+coeff_15 <- c("(Intercept)" = 1.34,
+              "phase" = -0.45,
+              "phase_sq" = -0.45,
+              "interview" = 0.2,
+              "conditionstandard" = 0.4,
+              "phase:interview" = 0,
+              "phase_sq:interview" = 0,
+              "phase:conditionstandard" = -0.2,
+              "phase_sq:conditionstandard" = -0.2,
+              "interview:conditionstandard" = 0.2,
+              "phase:interview:conditionstandard" = 0.3,
+              "phase_sq:interview:conditionstandard" = 0.15)
+
+sim_lmer_15 <- makeLmer(
+  formula = y ~ phase
+  + phase_sq
+  + interview
+  + interview*condition
+  + phase*interview
+  + phase_sq*interview
+  + phase*condition
+  + phase_sq*condition
+  + phase*interview*condition
+  + phase_sq*interview*condition
+  + (1|id), 
+  fixef = coeff_15,
+  VarCorr = 1.6,
+  sigma = 1.2,
+  data = structure_full
+)
+
+sim_15 <- powerSim(sim_lmer_15,
+                   test = fixed("phase_sq:interview:conditionstandard",
+                   method = "t"),
+                   nsim = 1000)
+
+save(sim_15, file = "sim_sos_15.rda")
